3709f7c1b15df498f426796b36ed6371c67ed1e2
Kdaniel-prog <74417423+Kdaniel-prog@users.noreply.github.com>
2022-05-25 17:31:34 +0200

    rendering correctly

--- todo-frontend/src/column.jsx
+++ todo-frontend/src/column.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+
+//stílusok
+const Container = styled.div`
+  margin: 8px;
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+`;
+const Title = styled.h3`
+  padding: 8px;
+`;
+const TaskList = styled.div`
+  padding: 8px;
+`;
 
 export default class Column extends React.Component{
-    render(){
-        return this.props.Column.title;
+    render() {
+        return (
+            <Container>
+                <Title>{this.props.column.title}</Title>
+                <TaskList>Task go here</TaskList>
+            </Container>
+        )
     }
 }
